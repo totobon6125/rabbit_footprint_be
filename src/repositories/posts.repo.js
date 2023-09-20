274b9534fefe8538359d7b9eb91dc0e93706b4a6
MTMLABS <gusdnjs14@gmail.com>
2023-09-20 10:31:47 +0900

    shw test

--- src/repositories/posts.repo.js
+++ src/repositories/posts.repo.js
@@ -1,28 +1,73 @@
-import { prisma } from '../utils/prisma/index.js'
+// src/repositories/posts.repo.js
+
+import { prisma } from "../utils/prisma/index.js";
 
 export class PostsRepo {
-    //* 게시글 생성 API
-    createPost = async (userId, content) => {
-        const createdPost = await prisma.posts.create({
-            data: {
-                UserId: userId,
-                content
-            }
-        });
-        return createdPost;
-    };
+  // (1) 게시글 생성 API
+  createPost = async (userId, content) => {
+    const createdPost = await prisma.posts.create({
+      data: {
+        UserId: userId,
+        receiverId: +receiverId,
+        relationship,
+        content,
+      },
+    });
+    return createdPost;
+  };
 
+  // (2) 내게 쓴 게시글 조회 API
+  findPostWrittenToMeById = async (receiverId) => {
+    const posts = await prisma.posts.findMany({
+      where: { receiverId: +receiverId },
+      select: {
+        postId: true,
+        receiverId: true,
+        content: true,
+        createdAt: true,
+        updatedAt: true,
+        User: {
+          select: {
+            nickname: true,
+          },
+        },
+      },
+    });
 
-    //* 게시글 조회 API
-    findAllPosts = async (userId) => {
-        const posts = await prisma.posts.findMany({
-            where: {
-                UserId: userId
-            }
-        });
+    return posts;
+  };
 
-        return posts;
-    }
+  // (3) 내가 쓴 게시글 조회 API
+  findPostIWroteById = async (userId) => {
+    const posts = await prisma.posts.findMany({
+      where: { UserId: +userId },
+      select: {
+        postId: true,
+        content: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
 
+    return posts;
+  };
 
+  // (4) 게시글 수정 API
+  updatePost = async (postId, password, relationship, content, receiverId) => {
+    // ORM인 Prisma에서 Posts 모델의 update 메서드를 사용해 데이터를 수정합니다.
+    const updatedPost = await prisma.posts.update({
+      where: {
+        postId: +postId,
+        password: password,
+      },
+      data: {
+        relationship,
+        content,
+        receiverId,
+        editCount: { increment: 1 },
+      },
+    });
+
+    return updatedPost;
+  };
 }
--- src/routes/posts.router.js
+++ src/routes/posts.router.js
@@ -1,3 +1,5 @@
+// src/routes/posts.router.js
+
 import express from 'express';
 
 import { PostsCtr } from '../controllers/posts.ctr.js'
@@ -6,10 +8,17 @@ import authMiddleware from '../middlewares/auth.middleware.js';
 const router = express.Router();
 const postsController = new PostsCtr(); // PostsController 를 인스턴스화 시킨다.
 
-// 게시글 생성 API
+// (1) 게시글 생성 API
 router.post('/posts', authMiddleware, postsController.createPost); // 요청이 들어오면 해당하는 컨트롤러로 요청을 전달함.
 
-// 게시글 조회 API
-router.get('/posts', authMiddleware, postsController.getPosts);
+// (2) 내가 받은 게시글 조회 API
+router.get('/receive/:receiverId', authMiddleware, postsController.getPostsWrittenToMe);
+
+// (3) 내가 쓴 게시글 조회 API 
+router.get('/posts/:writerId', authMiddleware, postsController.getPostsIWrote);
+
+// (4) 내가 쓴 게시글 1회 수정 API 
+router.put('/posts/:postId', authMiddleware, postsController.updatePost);
+
 
 export default router;
