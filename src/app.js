e5432648e1269728cb5f681f309ce157bd1f4d3c
totobon <totobon6125@gmail.com>
2023-09-21 18:36:56 +0900

    "test"

--- src/app.js
+++ src/app.js
@@ -1,15 +1,22 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import cors from 'cors';
+
+
 import indexRouter from "./routes/index.js";
 
-// .env 설치 해야 배포할 때 문제가 안 생김.
+
+dotenv.config()
 
 const app = express();
-const PORT = 3306;
+const PORT = process.env.PORT;
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(cors());
 
 app.get("/", (req, res) => {
   return res.json({ message: "환영 합니당!@." });
--- src/controllers/signin.ctr.js
+++ src/controllers/signin.ctr.js
@@ -7,10 +7,10 @@ export class SignInCtr {
     signIn = async (req, res, next) => {
         try {
             const { email, password } = req.body;
-
             const token = await this.signInService.signIn(email, password);
 
-            res.cookie('Authorization', `Bearer ${token}`);
+            //? cookie 매서드가 아닌 header 매서드를 사용해야 FE가 res.header 에서 받아서 사용할 수 있다.
+            res.header('Authorization', `Bearer ${token}`);
 
             return res.status(200).json({ Message: "로그인 성공" });
         } catch (err) {
@@ -31,6 +31,7 @@ export class SignInCtr {
         }
     }
 
+    // 토큰 확인 용 
     token = async (req, res, next) => {
         try {
             const token = req.headers.cookie;
