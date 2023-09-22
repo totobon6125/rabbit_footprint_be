9cc07ff610c766c9fab7ddb5b2a3ca183edc29b2
totobon <totobon6125@gmail.com>
2023-09-22 22:19:45 +0900

    db 수정!!!!

--- src/controllers/signup.ctr.js
+++ src/controllers/signup.ctr.js
@@ -6,15 +6,18 @@ export class SignUpCtr {
     //# 회원 가입
     signUp = async (req, res, next) => {
         try {
-            const { email, password, confirm } = req.body;
+            const { email, nickname, password, confirm } = req.body;
 
             //!email 형태 확인
             const emailRegex = new RegExp(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/);
             if (!emailRegex.test(email)) { return res.status(400).json({ errorMessage: 'email 조건에 맞지 않습니다.' }) }
-            
+
             //! email 중복확인
             await this.signUpService.findUserByEmail(email);
             // 레포지토리에서 출발한 error 는 try-catch 를 통해 catch(err) 로 잡혀 갑니다.
+
+            //! nickname 중복확인
+            await this.signUpService.findUserByNickName(nickname);
 
             //! 비밀번호 확인
             const passwordRegex = new RegExp(/^(?=.*[a-zA-Z])(?=.*[0-9]).{4,}$/i);
@@ -22,7 +25,7 @@ export class SignUpCtr {
             if (!passwordRegex.test(password)) { return res.status(400).json({ errorMessage: 'password 조건에 맞지 않습니다.' }) }
             if (password !== confirm) { return res.status(412).json({ errorMessage: '패스워드와 확인 이 다릅니다.' }) };
 
-            await this.signUpService.signUp(email, password);
+            await this.signUpService.signUp(email, nickname, password);
 
             return res.status(201).json({ message: '회원가입에 성공하였습니다.' });
 
--- src/repositories/signup.repo.js
+++ src/repositories/signup.repo.js
@@ -4,31 +4,52 @@ import { prisma } from '../utils/prisma/index.js'
 import { CustomError } from '../errors/customError.js'
 
 export class SignUpRepo {
-    // 회원 가입
-    signUp = async (email, password) => {
+    //# 회원 가입
+    signUp = async (email, nickname, password) => {
         // 비밀번호 해싱
         const hashedPassword = await bcrypt.hash(password, 10);
 
-        // 회원 가입
-        const signUp = await prisma.users.create({
+        // 사용자 생성
+        const user = await prisma.users.create({
             data: {
                 email,
                 password: hashedPassword,
             },
         });
 
-        return signUp;
-    };
+        // 사용자 정보 생성 및 사용자와 연결
+        const userInfo = await prisma.userInfos.create({
+            data: {
+                nickname,
+                User: { connect: { userId: user.userId } }
+            },
+        });
 
+        return { user, userInfo };
+    }
+    
     //! 회원가입 email 중복확인
     findUserByEmail = async (email) => {
         const eMail = await prisma.users.findUnique({
             where: { email }
         });
         if (eMail) { // 이 에러는 return으로 빠지는 것이 아닌 throw 를 통해 서비스 계층으로 이동 합니다.
-            throw new CustomError(412, '중복된 닉네임입니다')
+            throw new CustomError(412, '중복된 email 입니다')
         };
 
         return eMail;
+    };
+
+
+    //! 회원가입 nickname 중복확인
+    findUserByNickName = async (nickname) => {
+        const nickName = await prisma.UserInfos.findUnique({
+            where: { nickname }
+        });
+        if (nickName) { // 이 에러는 return으로 빠지는 것이 아닌 throw 를 통해 서비스 계층으로 이동 합니다.
+            throw new CustomError(412, '중복된 닉네임입니다')
+        };
+
+        return nickName;
     };
 };
