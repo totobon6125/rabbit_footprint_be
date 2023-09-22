90a364744724e11cc600a7fce4e358e676499fe1
totobon <totobon6125@gmail.com>
2023-09-22 09:56:46 +0900

    rf token

--- src/controllers/signin.ctr.js
+++ src/controllers/signin.ctr.js
@@ -7,6 +7,24 @@ export class SignInCtr {
     signIn = async (req, res, next) => {
         try {
             const { email, password } = req.body;
+
+            const token = await this.signInService.signIn(email, password);
+            console.log(token.accessToken)
+            //? cookie 매서드가 아닌 header 매서드를 사용해야 FE가 res.header 에서 받아서 사용할 수 있다.
+            res.header('accessToken', `${token.accessToken}`);
+            res.header('refreshToken', `${token.refreshToken}`);
+
+            /* res.cookie('Authorization', `Bearer ${token}`);
+               res.cookie 는 FE와 BE 도메인이 같은 경우에만 가능하다 */
+
+            return res.status(200).json({ Message: "로그인 성공" });
+        } catch (err) {
+            next(err)
+        }
+    }
+/*     signIn = async (req, res, next) => {
+        try {
+            const { email, password } = req.body;
             const token = await this.signInService.signIn(email, password);
 
             //? cookie 매서드가 아닌 header 매서드를 사용해야 FE가 res.header 에서 받아서 사용할 수 있다.
@@ -16,29 +34,29 @@ export class SignInCtr {
         } catch (err) {
             next(err)
         }
-    }
+    } */
 
-    //# 로그 아웃 API
-    signOut = async (req, res, next) => {
-        try {
-            console.log(req.headers.cookie)
-            const token = req.headers.cookie.split(' ')[1];
-            console.log(token)
+    // // 로그 아웃 API
+    // signOut = async (req, res, next) => {
+    //     try {
+    //         console.log(req.headers.cookie)
+    //         const token = req.headers.cookie.split(' ')[1];
+    //         console.log(token)
 
-            return res.status(200).json({ Message: "로그아웃 성공" });
-        } catch (err) {
-            res.status(400).json({ errMessage: '이미 로그아웃 하셨습니다.' });
-        }
-    }
+    //         return res.status(200).json({ Message: "로그아웃 성공" });
+    //     } catch (err) {
+    //         res.status(400).json({ errMessage: '이미 로그아웃 하셨습니다.' });
+    //     }
+    // }
 
-    // 토큰 확인 용 
-    token = async (req, res, next) => {
-        try {
-            const token = req.headers.cookie;
-            console.log("토큰 확인", token)
-            return res.status(200).json({ Message: "토큰값 확인용" });
-        } catch (err) {
-            res.status(400).json({ errMessage: '확인 안됨 에러!!' });
-        }
-    }
+    // // 토큰 확인 용 
+    // token = async (req, res, next) => {
+    //     try {
+    //         const token = req.headers.cookie;
+    //         console.log("토큰 확인", token)
+    //         return res.status(200).json({ Message: "토큰값 확인용" });
+    //     } catch (err) {
+    //         res.status(400).json({ errMessage: '확인 안됨 에러!!' });
+    //     }
+    // }
 }
--- src/repositories/signin.repo.js
+++ src/repositories/signin.repo.js
@@ -1,28 +1,46 @@
 import jwt from 'jsonwebtoken';
 
 import { prisma } from '../utils/prisma/index.js'
-import { CustomError } from "../errors/customError.js";
-
+import { CustomError } from '../errors/customError.js'
 
 export class SignInRepo {
 
     //# 로그인
-    signIn = async (email, password) => {
-        const signIn = await prisma.users.findUnique({
-            where: { email }
+
+    // Access, Refresh Token을 생성합니다.
+    // 토큰을 생성하는 함수
+    generateTokens = (userId) => {
+        const accessToken = jwt.sign({ userId }, process.env.AC_KEY, {
+            expiresIn: '10m',
         });
 
-        if (!signIn) {
-            throw new CustomError(412, 'email 을 확인해주세요')
-        } else if (signIn.password !== password) {
-            throw new CustomError(412, '패스워드를 확인해주세요')
-        };
+        const refreshToken = jwt.sign({ userId }, process.env.RF_KEY, {
+            expiresIn: '7d',
+        });
 
-        const token = jwt.sign({ userId: signIn.userId },
-            process.env.jwt_key,
-            { expiresIn: '10m' }
-        );
-        return token;
+        return { accessToken, refreshToken };
+    };
+
+    // 이메일과 비밀번호로 로그인하고 토큰을 반환합니다
+    signIn = async (email, password) => {
+        const user = await prisma.users.findUnique({
+            where: { email },
+        });
+
+        if (!user) {
+            throw new CustomError(412, '이메일을 찾을 수 없습니다');
+        } else if (user.password !== password) {
+            throw new CustomError(412, '잘못된 비밀번호입니다');
+        }
+
+        // 토큰을 생성합니다
+        const { accessToken, refreshToken } = this.generateTokens(user.userId);
+
+        // 리프레시 토큰을 사용자 데이터에 저장 합니다.
+        await prisma.users.update({ where: { userId: user.userId }, data: { refreshToken } });
+
+        return { accessToken, refreshToken };
     };
 
 };
+
