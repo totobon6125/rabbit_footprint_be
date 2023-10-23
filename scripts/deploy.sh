2a18d2e15d5c042592015df2b360946311140a99
totobon <totobon6125@gmail.com>
2023-10-23 16:44:08 +0900

    cd test

---
+++ scripts/deploy.sh
@@ -0,0 +1,22 @@
+# 프로젝트 위치 설정
+REPOSITORY=/home/ubuntu/rabbit_footprint_be
+
+# 프로젝트 위치로 이동
+cd $REPOSITORY
+
+# 의존성 설치
+echo "> install dependency"
+npm install
+
+# 프로젝트 build
+echo "> build application"
+nest build
+
+# pm2 실행 또는 리로드
+if pm2 list | grep -q "rabbit-footprint"; then
+  echo "> reload application"
+  pm2 reload rabbit-footprint
+else
+  echo "> start application"
+  pm2 start dist/src/main.js --name rabbit-footprint
+fi
